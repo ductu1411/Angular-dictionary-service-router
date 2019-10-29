a77a6499640e00c3502a03733783827ca4b81644
ductu1411 <ductu1411@gmail.com>
2019-10-30 03:06:16 +0700

    Add dictionary page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DictionaryComponent } from './dictionary/dictionary.component';
+import { DictionaryPageComponent } from './dictionary-page/dictionary-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DictionaryComponent
+    DictionaryComponent,
+    DictionaryPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dictionary-page/dictionary-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface IWord {
+  key: string;
+  meaning: string;
+}
+
+@Component({
+  selector: 'app-dictionary-page',
+  templateUrl: './dictionary-page.component.html',
+  styleUrls: ['./dictionary-page.component.scss']
+})
+export class DictionaryPageComponent implements OnInit {
+  private listWord: IWord[] = [
+    {key: 'service', meaning: 'dịch vụ'},
+    {key: 'service provider', meaning: 'nhà cung cấp dịch vụ'},
+    {key: 'provide', meaning: 'cung cấp'},
+    {key: 'efficient', meaning: 'hiệu quả'},
+    {key: 'snow', meaning: 'tuyết'}
+  ];
+
+  private word: IWord;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
